8004b0b865b67e4ed055214f804a1eef8cbbc196
gitzno <hoangthuy.forjob@gmail.com>
2023-05-24 09:59:30 +0700

    first commit

---
+++ src/main/java/com/glassbridge/Glass/Bridge/GlassBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.glassbridge.Glass.Bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glassbridge/Glass/Bridge/GlassBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glassbridge.Glass.Bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
